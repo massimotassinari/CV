e6d2c218c4855194f9aa9d3c907eaff50244f37c
massimo tassinari <massimo@MacBook-Pro-de-massimo.local>
2022-02-12 12:48:18 -0400

    nav bar and images

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+//funcion que me desplega el menu bar
+
+function action(){
+    console.log('hola');
+    var ancla = document.getElementsByClassName('nav-enlace');
+    for(var i=0;i<ancla.length;i++){
+        ancla[i].classList.toggle('desaparecer');
+    }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
